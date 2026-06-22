e9c1b5b5ab983ab3efd41fd6dc4f2f802af620ac
David Blume <david@blumecloud.de>
2026-06-22 19:02:19 +0200

    ci: setup renovate in github actions

---
+++ .github/renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  username: 'renovate-bot',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: true,
+  platform: 'github',
+  repositories: ['the-blumworx/onboordo'],
+};
