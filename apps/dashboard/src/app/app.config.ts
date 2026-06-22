6905cc0dcc304ec1e151786879d458ca48435284
David Blume <david@blumecloud.de>
2026-06-22 06:21:01 +0200

    build: setup angular material and tailwind v4 for dashboard

--- apps/dashboard/src/app/app.config.ts
+++ apps/dashboard/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes)
+    provideRouter(appRoutes),
+    provideAnimationsAsync()
   ]
 };
